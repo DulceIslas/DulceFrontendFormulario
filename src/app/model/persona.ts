015d438355a7a692fa1ef33af8092ba3395462d2
IslasDulce <dulcesitoisa79@gmail.com>
2021-10-29 14:07:27 -0500

    DulceFrontendFormulario

---
+++ src/app/model/persona.ts
@@ -0,0 +1,12 @@
+import { Pais } from "./pais";
+
+
+export class Persona{
+  idPersona!: number;
+  nombres!: string;
+  apellidos!: string;
+  edad!: number;
+  pais!: Pais;
+  sexo!: string; 
+  fecha_nacimiento!: string; 
+}
